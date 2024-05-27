f3d07806e921642ea276a9870228ca8a58683eb5
Nishant Kishore <ynm2@cdc.gov>
2024-05-27 06:57:31 -0400

    created framework for new fx

---
+++ create_afp_map.R
@@ -0,0 +1,15 @@
+#' Function to create AFP map
+#' @description This function does something great
+#' @import ggplot2 dplyr
+#' @param afp.data tibble: contains all AFP information
+#' @param ctry str: specify country of interest
+#' @return ggplot map
+create_afp_map <- function(afp.data, ctry){
+
+  #read AFP data
+  #subset to ctry of interest
+  #plot afp cases
+  #write out figure
+
+
+}
